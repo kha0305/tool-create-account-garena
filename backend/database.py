ee8d8258d4af37cd7e74eeec967b7d1fe9a52644
kha0305 <baokha0305@gmail.com>
2025-11-03 20:24:51 +0700

    edit

--- backend/database.py
+++ backend/database.py
@@ -21,7 +21,7 @@ class MySQLDatabase:
             host = os.environ.get('MYSQL_HOST', 'localhost')
             port = int(os.environ.get('MYSQL_PORT', '3306'))
             user = os.environ.get('MYSQL_USER', 'root')
-            password = os.environ.get('MYSQL_PASSWORD', '')
+            password = os.environ.get('MYSQL_PASSWORD', '190705')
             database = os.environ.get('MYSQL_DATABASE', 'garena_creator_db')
             
             self.pool = await aiomysql.create_pool(
